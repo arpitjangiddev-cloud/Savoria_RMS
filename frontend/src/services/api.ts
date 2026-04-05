ab0e9288f6053c289cb45e0f6c6355d6858c3295
arpitjangiddev-cloud <arpitjangid.dev@gmail.com>
2026-04-06 01:52:17 +0530

    Fix: remove 401 interceptor redirect that prevented fallback data from loading

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -11,13 +11,7 @@ api.interceptors.request.use((config) => {
 
 api.interceptors.response.use(
   (res) => res,
-  (err) => {
-    if (err.response?.status === 401) {
-      localStorage.clear();
-      window.location.href = '/login';
-    }
-    return Promise.reject(err);
-  }
+  (err) => Promise.reject(err)
 );
 
 export default api;
